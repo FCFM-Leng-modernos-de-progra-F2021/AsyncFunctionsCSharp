645c3fc4bd6b2613d223561a733c0de12ce914c5
Rolando <rolando.fcfm2020@gmail.com>
2021-03-24 19:44:28 -0600

    first commit

---
+++ AsyncFunctionsCSharp/Functions/Counter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncFunctionsCSharp.Functions
+{
+    public class Counter
+    {
+
+        public async Task<string> PrintNumbers()
+        {
+
+            for (int i = 0; i < 200; i++)
+            {
+
+                Console.WriteLine(i);
+            }
+
+            return await Task.FromResult("Ya terminé de imprimir los numeros en la función PrintNumbers");
+        }
+    }
+}
---
+++ AsyncFunctionsCSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AsyncFunctionsCSharp.Functions;
+
+namespace AsyncFunctionsCSharp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+            string stringTask = "";
+            bool secondaryTaskFinished = false;
+
+            Task.Run(async () =>
+            {
+                //Este es el segundo hilo de mi programa
+                stringTask = await counter.PrintNumbers();
+
+            }).ContinueWith(cont =>
+            {
+                Console.WriteLine("Terminó la tarea en segundo plano!!");
+                secondaryTaskFinished = true;
+            });
+
+            Console.WriteLine("Este es el hilo principal de mi programa");
+
+            while (!secondaryTaskFinished)
+                Console.WriteLine("Esperate, todavía no termino!! D:");
+
+
+        }
+    }
+}
